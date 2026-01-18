203a9128107c214c94d90f2be628cbdfa8d1323e
pommeJedusor <118850425+pommeJedusor@users.noreply.github.com>
2026-01-18 20:38:00 +0100

    cli

--- src/main.rs
+++ src/main.rs
@@ -1,132 +1,67 @@
-const BIN_TO_CHAR: [&str; 128] = [
-    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
-    "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=",
-    "[", "]", ";", "'", "#", "|", ",", ".", "/", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ",
-    " ", " ", " ", " ", " ", " ", " ", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
-    "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "!", "€", "£", "$", "%",
-    "^", "&", "*", "(", ")", "_", "+", "{", "}", ":", "@", "~", "|", "<", ">", "?", ")", "\"", " ",
-    " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "", "",
-];
+use std::env;
+pub mod cat_translater;
 
-const BIN_TO_CAT: [&str; 16] = [
-    "meow", "meoww", "meowww", "meowwww", "mrow", "mroww", "mrowww", "mrowwww", "mrrp", "mrrrp",
-    "mrrrrp", "mrrrrrp", "purr", "purrr", "purrrr", "purrrrr",
-];
+const CAT_TO_TEXT_SUB_COMMAND_NAME: &str = "decrypt";
+const TEXT_TO_CAT_SUB_COMMAND_NAME: &str = "crypt";
 
-fn cat_noise_to_bin(cat_noise: &str) -> usize {
-    if cat_noise.get(0..1).unwrap() == "p" {
-        // length min bound to 4
-        let len = usize::max(cat_noise.len(), 4);
-        // length max bound to 7
-        let len = usize::min(len, 7);
-        return 12 | (len - 4);
-        // if meow
-    } else if cat_noise.get(0..1).unwrap() == "m" && cat_noise.contains("e") {
-        // length min bound to 4
-        let len = usize::max(cat_noise.len(), 4);
-        // length max bound to 7
-        let len = usize::min(len, 7);
-        return len - 4;
-        // if mrrp
-    } else if cat_noise.get(0..1).unwrap() == "m" && !cat_noise.contains("o") {
-        // length min bound to 4
-        let len = usize::max(cat_noise.len(), 4);
-        // length max bound to 7
-        let len = usize::min(len, 7);
-        return 8 | (len - 4);
-        // if mrow
-    } else {
-        // length min bound to 4
-        let len = usize::max(cat_noise.len(), 4);
-        // length max bound to 7
-        let len = usize::min(len, 7);
-        return 4 | (len - 4);
-    }
-}
-
-// translate a {length} bit number to its String binary representation with a length of {length} bits
-fn number_to_bin(number: u8, length: usize) -> String {
-    let result = format!("{number:b}");
-    "0".repeat(length - result.len()) + &result
-}
-
-// translate a text to its bit representation, each character is 7 bits and their bit
-// representation is their index in the BIN_TO_CHAR array
-fn text_to_bin(text: &str) -> String {
-    text.chars()
-        .map(|x| {
-            BIN_TO_CHAR
-                .iter()
-                .enumerate()
-                .find(|y| y.1 == &x.to_string())
-                .map(|x| x.0)
-                .unwrap()
-        })
-        .map(|x| number_to_bin(x as u8, 7))
-        .collect::<Vec<String>>()
-        .join("")
-}
-
-// translate cat noises to their bit representations, each noise is 4 bits and their bit
-// representation is their index in the BIN_TO_CAT array
-fn cat_to_bin(text: &str) -> String {
-    text.split(" ")
-        .filter(|x| x != &":3" && x != &":3c")
-        .map(|x| cat_noise_to_bin(x))
-        .map(|x| number_to_bin(x as u8, 4))
-        .collect::<Vec<String>>()
-        .join("")
-        + &text
-            .split(" ")
-            .filter(|x| x == &":3" || x == &":3c")
-            .map(|x| if x == ":3" { 0 } else { 1 })
-            .map(|x| number_to_bin(x, 1))
-            .collect::<Vec<String>>()
-            .join("")
-}
-
-fn bin_to_cat(bin: &str) -> String {
-    let mut result = vec![];
-    for i in (0..bin.len()).filter(|x| x % 4 == 0 || bin.len() - x <= bin.len() % 4) {
-        if bin.len() - i <= bin.len() % 4 {
-            result.push(if bin.get(i..=i).unwrap() == "0" {
-                ":3"
-            } else {
-                ":3c"
-            })
-        } else {
-            let cat_bin = bin.get(i..i + 4).unwrap();
-            let cat_noise = BIN_TO_CAT[usize::from_str_radix(cat_bin, 2).expect("")];
-            result.push(cat_noise);
-        }
-    }
-    result.join(" ")
-}
-
-fn bin_to_text(bin: &str) -> String {
-    let mut result = vec![];
-    for i in (0..bin.len())
-        .filter(|x| x % 7 == 0)
-        .filter(|x| bin.len() >= x + 7)
-    {
-        let letter_bin = bin.get(i..i + 7).unwrap();
-        let letter = BIN_TO_CHAR[usize::from_str_radix(letter_bin, 2).expect("")];
-        result.push(letter);
-    }
-    result.join("")
-}
-
-fn text_to_cat(text: &str) -> String {
-    bin_to_cat(&text_to_bin(text))
-}
-
-fn cat_noises_to_text(cat_noises: &str) -> String {
-    bin_to_text(&cat_to_bin(cat_noises))
+fn show_help(program_name: &str) {
+    println!(
+        "some basic command examples:\n{} {} meoww meow mrow purrr mrowwww mrrrp mrowww meoww purr mrow mrow mroww meoww meowww mrrrrrp :3 :3c :3\n{} {} it works!\n\nalternatively to crypt or decrypt multiple times you can use the -d flag putting the depth afterward (if you want to crypt it two times -d 2)\n{} {} -d 2\n\nyou can use the --help or -h to get this message",
+        program_name,
+        CAT_TO_TEXT_SUB_COMMAND_NAME,
+        program_name,
+        TEXT_TO_CAT_SUB_COMMAND_NAME,
+        program_name,
+        TEXT_TO_CAT_SUB_COMMAND_NAME
+    );
 }
 
 fn main() {
-    let text = "abcdefghijklmnopqrstuvwxyz";
-    let cat_noise = "meow meow meow mrow meoww meow meowwww meow mrrp meoww mrow meowwww meow mrowwww meoww meow meowww mrow mroww meow mrrrrrp meoww mrrp meowwww mrow mrowwww meow purrrrr meowww meow mrow mrow mrrrp meoww meowwww meowww mrrp mroww mrow mrrrrrp meoww mrowwww meowwww meow mrowww :3 :3c";
-    println!("{}", text_to_cat(text));
-    println!("{}", cat_noises_to_text(cat_noise));
+    let mut depth = 1;
+    let mut text_starting_index = 2;
+
+    let args: Vec<String> = env::args().collect();
+    let program_name = &args[0];
+
+    // if not sub command in second place nor help command
+    if args.len() <= 1
+        || (&args[1] != CAT_TO_TEXT_SUB_COMMAND_NAME
+            && &args[1] != TEXT_TO_CAT_SUB_COMMAND_NAME
+            && &args[1] != "-h"
+            && &args[1] != "--help")
+    {
+        println!(
+            "the comamnd must include one of those two sub commands {}, {}\n",
+            CAT_TO_TEXT_SUB_COMMAND_NAME, TEXT_TO_CAT_SUB_COMMAND_NAME,
+        );
+        show_help(program_name);
+        return;
+    }
+
+    let subcommand = &args[1];
+
+    if subcommand == "-h" || subcommand == "--help" {
+        show_help(program_name);
+        return;
+    }
+
+    // depth flag
+    if args.len() >= 4 && &args[2] == "-d" && args[3].parse::<u16>().is_ok() {
+        depth = args[3].parse::<u16>().unwrap();
+        text_starting_index = 4;
+    }
+
+    // decrypt
+    if subcommand == CAT_TO_TEXT_SUB_COMMAND_NAME {
+        let mut decrypted_message = args[text_starting_index..].join(" ");
+        (0..depth).for_each(|_| {
+            decrypted_message = cat_translater::cat_noises_to_text(&decrypted_message)
+        });
+        println!("{}", decrypted_message);
+        // crypt
+    } else {
+        let mut crypted_message = args[text_starting_index..].join(" ");
+        (0..depth).for_each(|_| crypted_message = cat_translater::text_to_cat(&crypted_message));
+        println!("{}", crypted_message);
+    }
 }
